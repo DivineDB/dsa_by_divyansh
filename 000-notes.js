b5813fc4730a3daa6135b130f432348816e6a867
Divyansh Baghel <divyanshbaghel456@gmail.com>
2025-07-16 02:46:32 +0530

    Remove Notes File  from repository

--- 000-notes.js
+++
@@ -1,9 +0,0 @@
-//Writing my thoughts.
-
-const isPalindrome = str => str === str.split("").reverse().join("")
-? "Palindrome" : "Not a Palindrome";
-
-console.log(isPalindrome("abba"));
-console.log(isPalindrome("divyansh"));
-
-
