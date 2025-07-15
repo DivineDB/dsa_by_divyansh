37eb6c1d73ddd26e1b8070eec049abdb922faf7d
Divyansh Baghel <divyanshbaghel456@gmail.com>
2025-07-16 00:59:37 +0530

    Reversing a string using JS methods.

---
+++ 000-notes.js
@@ -0,0 +1,10 @@
+// ReverseString
+
+// reverseString = (str) =>
+//     str.split("");
+
+// console.log(reverseString("Jaishree"));
+
+ reverse = (str) => str.split("").reverse().join("");
+
+console.log(reverse("divyansh"));
