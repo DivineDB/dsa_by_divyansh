221223f50a280bc48403256b03ead1a5c220a00d
Divyansh Baghel <divyanshbaghel456@gmail.com>
2025-07-16 18:49:53 +0530

    Add .gitignore to exclude log and temporary files; create initial notes with palindrome checker

---
+++ 000-notes.js
@@ -0,0 +1,9 @@
+//Writing my thoughts.
+
+const isPalindrome = str => str === str.split("").reverse().join("")
+? "Palindrome" : "Not a Palindrome";
+
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("divyansh"));
+
+
